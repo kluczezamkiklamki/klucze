d852b5b2ee3e7e6fbdbd18940dc40434256f1656
Valkon <96625039+Nawija@users.noreply.github.com>
2023-06-19 09:10:12 +0200

    robot.txt sidemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
             },
         },
         `gatsby-plugin-sitemap`,
+        `gatsby-plugin-robots-txt`,
         {
             resolve: `gatsby-transformer-remark`,
             options: {},
